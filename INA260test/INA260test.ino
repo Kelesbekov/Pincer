5a8c99ed2c131c45a285bcecf6f4c92798df137e
Kelesbekov <118713007+Kelesbekov@users.noreply.github.com>
2023-11-06 11:21:13 +1100

    Upload INA260 test script

---
+++ INA260test/INA260test.ino
@@ -0,0 +1,44 @@
+#include <Adafruit_INA260.h>
+#include <ESP32Servo.h> 
+
+Servo myservo1;
+int servo1Pin = MOSI;
+int servo1ReadPin = A3;
+int servo1Read;
+int angle1 = 40;
+
+
+
+Adafruit_INA260 ina260 = Adafruit_INA260();
+
+void setup() {
+  Serial.begin(115200);
+  // Wait until serial port is opened
+  while (!Serial) { delay(10); }
+
+  Serial.println("Adafruit INA260 Test");
+
+  if (!ina260.begin()) {
+    Serial.println("Couldn't find INA260 chip");
+    while (1);
+  }
+  Serial.println("Found INA260 chip");
+  myservo1.attach(servo1Pin, 500, 2400);
+  myservo1.write(angle1);
+}
+
+void loop() {
+  Serial.print("Current:");
+  Serial.print(ina260.readCurrent());
+  //Serial.println(" mA");
+  Serial.print("\t");
+  Serial.print("Bus Voltage:");
+  Serial.print(ina260.readBusVoltage());
+  //Serial.println(" mV");
+  Serial.print("\t");
+  Serial.print("Power:");
+  Serial.println(ina260.readPower());
+  //Serial.println(" mW");
+
+  delay(50);
+}
