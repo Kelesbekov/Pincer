8e79abd488ff066b45d307c745158bc46b38f321
Kelesbekov <118713007+Kelesbekov@users.noreply.github.com>
2023-11-06 13:48:37 +1100

    Update INA260test.ino

--- INA260test/INA260test.ino
+++ INA260test/INA260test.ino
@@ -1,5 +1,8 @@
 #include <Adafruit_INA260.h>
 #include <ESP32Servo.h> 
+#include <movingAvg.h>
+
+movingAvg avgCurrentObject(10);
 
 Servo myservo1;
 int servo1Pin = MOSI;
@@ -25,19 +28,25 @@ void setup() {
   Serial.println("Found INA260 chip");
   myservo1.attach(servo1Pin, 500, 2400);
   myservo1.write(angle1);
+  avgCurrentObject.begin();
 }
 
 void loop() {
-  Serial.print("Current:");
-  Serial.print(ina260.readCurrent());
+  int readCurrent = ina260.readCurrent();
+  int avgCurrent = avgCurrentObject.reading(readCurrent);
+  Serial.print("AverageCurrent:");
+  Serial.print(avgCurrent);
+  Serial.print("\t");
+  Serial.print("ReadingCurrent:");
+  Serial.println(readCurrent);
   //Serial.println(" mA");
-  Serial.print("\t");
-  Serial.print("Bus Voltage:");
-  Serial.print(ina260.readBusVoltage());
-  //Serial.println(" mV");
-  Serial.print("\t");
-  Serial.print("Power:");
-  Serial.println(ina260.readPower());
+  // Serial.print("\t");
+  // Serial.print("Bus Voltage:");
+  // Serial.print(ina260.readBusVoltage());
+  // //Serial.println(" mV");
+  // Serial.print("\t");
+  // Serial.print("Power:");
+  // Serial.println(ina260.readPower());
   //Serial.println(" mW");
 
   delay(50);
